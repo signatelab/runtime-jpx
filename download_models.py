36578e11b2f4df75923e76fb36ddb2d602bb1a4d
t.kusumoto <t.kusumoto@signate.co.jp>
2021-03-16 20:27:33 +0900

    modify Dockerfile

--- download_models.py
+++ download_models.py
@@ -1,3 +1,5 @@
 import transformers
+from transformers import BertJapaneseTokenizer
 
 transformers.BertModel.from_pretrained("cl-tohoku/bert-base-japanese-whole-word-masking")
+BertJapaneseTokenizer.from_pretrained("cl-tohoku/bert-base-japanese-whole-word-masking")
